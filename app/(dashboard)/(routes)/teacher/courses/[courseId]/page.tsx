a10d4edbfd97fdeaadddd95fe08a1c71f992bc94
SulajAcharya <139706131+SulajAcharya@users.noreply.github.com>
2024-02-27 23:44:38 +0530

    Course Category Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({
     params
@@ -26,6 +27,14 @@ const CourseIdPage = async ({
             id: params.courseId
         }
     });
+    
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
+        },
+    });
+
+    console.log(categories)
 
     if(!course){
         return redirect("/");
@@ -98,6 +107,14 @@ const CourseIdPage = async ({
                         }}
                         // initialData={{ description: course.description! }} // Using non-null assertion operator to indicate that course.description will never be null
                         courseId={course.id}
+                    />
+                    <CategoryForm
+                        initialData={course}
+                        courseId={course.id}
+                        options={categories.map((category) => ({
+                            label: category.name,
+                            value: category.id,
+                        }))}
                     />
                 </div>
             </div>
