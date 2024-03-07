8e76649f383c5f1162209f07e8065f8570824973
kartikunder <kartikofficial2361@gmail.com>
2024-03-08 00:40:52 +0530

    Chpater access from updated

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader, Loader2, PlusCircle } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation"; 
 import { Course, Chapter } from "@prisma/client";
@@ -65,8 +65,33 @@ export const ChaptersForm = ({
             toast.error("Something went wrong");
         }
     }
+
+    const onReorder = async (updateData: { id: string; position: number }[]) => {
+        try {
+          setIsUpdating(true);
+    
+          await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+            list: updateData
+          });
+          toast.success("Chapters reordered");
+          router.refresh();
+        } catch {
+          toast.error("Something went wrong");
+        } finally {
+          setIsUpdating(false);
+        }
+      }
+
+      const onEdit = (id: string) => {
+        router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+      }
     return(
-        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+                <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course chapters
                 <Button onClick={toggleCreating} variant="ghost">
@@ -119,8 +144,8 @@ export const ChaptersForm = ({
                 )}>
                     {!initialData.chapters.length && "No chapters"}
                     <ChaptersList
-                        onEdit = {() => {}}
-                        onReorder = {() => {}}
+                        onEdit = {onEdit}
+                        onReorder = {onReorder}
                         items = {initialData.chapters || []}
                     />
                 </div>
@@ -129,7 +154,7 @@ export const ChaptersForm = ({
                 <p className="text-xs text-muted-foreground mt-4">
                     Drag and drop to reorder the chapters
                 </p>
-            )}
+            )}                                                                  
         </div>
     ) 
 }
