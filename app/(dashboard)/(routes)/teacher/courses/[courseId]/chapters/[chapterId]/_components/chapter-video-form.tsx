bc6cb1e8266e9ba1f75ab803ce4c7ef9701f59a9
SulajAcharya <139706131+SulajAcharya@users.noreply.github.com>
2024-03-08 21:55:54 +0530

    Mux Initial

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -3,25 +3,26 @@
 import * as z from "zod";
 import axios from "axios";
 import { useState } from "react";
-import {  ImageIcon, Pencil, PlusCircle, Video } from "lucide-react";
+import {  ImageIcon, Pencil, PlusCircle, Video, VideoIcon } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation"; 
-import { Chapter, MuxData } from "@prisma/client";
+import { Chapter, Course, MuxData } from "@prisma/client";
+import Image from "next/image"; 
 
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
 
-interface ChapterVideoFormProps{
+interface ChapterVideoFormProps {
     initialData: Chapter & { muxData?: MuxData | null };
     courseId: string;
     chapterId: string;
-};
+  };
 
 const formSchema = z.object({
     videoUrl: z.string().min(1),
-  });
+});
 
-export const ChapterVideo = ({
+export const ChapterVideoForm = ({
     initialData,
     courseId,
     chapterId,
@@ -45,56 +46,58 @@ export const ChapterVideo = ({
     return(
         <div className="mt-6 border bg-slate-100 rounded-md p-4">
             <div className="font-medium flex items-center justify-between">
-                Chapter video
+                Chapter Video
                 <Button onClick={toggleEdit} variant="ghost">
-                {isEditing && (
-                    <>Cancel</>
-                )}
-                {!isEditing && !initialData.videoUrl && (
-                    <>
-                    <PlusCircle className="h-4 w-4 mr-2" />
-                    Add a video
-                    </>
-                )}
-                {!isEditing && initialData.videoUrl && (
-                    <>
-                    <Pencil className="h-4 w-4 mr-2" />
-                    Edit video
-                    </>
-                )}
+                    {isEditing && (
+                        <>Cancel</>
+                    )}
+                    {!isEditing && !initialData.videoUrl && (
+                        <>
+                            <PlusCircle className="h-4 w-4 mr-2"/>
+                            Add an Video
+                        </>
+                    )}
+                    {!isEditing && initialData.videoUrl && (
+                        <>
+                            <Pencil className="h-4 w-4 mr-2"/>
+                            Edit Video
+                        </>
+                    )}
                 </Button>
             </div>
-            {!isEditing && (
-                !initialData.videoUrl ? (
+            {!isEditing &&(
+                !initialData.videoUrl ?(
                     <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
-                        <Video className="h-10 w-10 text-slate-500" />
+                        <Video className="h-10 w-10 text-slate-500"/>
                     </div>
-                ) : (
+                )
+                // <p className={cn("text-sm mt-2", !initialData.description && "text-slate-500 italic")}>
+                //     {initialData.description || "No description"}
+                // </p>
+                 : (
                 <div className="relative aspect-video mt-2">
-                    <MuxPlayer
-                        playbackId={initialData?.muxData?.playbackId || ""}
-                    />
+                    Video uploaded
                 </div>
                 )
             )}
             {isEditing && (
                 <div>
-                    <FileUpload
+                    <FileUpload 
                         endpoint="chapterVideo"
                         onChange={(url) => {
-                        if (url) {
-                            onSubmit({ videoUrl: url });
-                        }
+                             if (url) {
+                                onSubmit({ videoUrl: url })
+                             }
                         }}
                     />
-                <div className="text-xs text-muted-foreground mt-4">
-                    Upload this chapter&apos;s video
-                </div>
+                    <div className="text-xs text-muted-foreground mt-4">
+                        Upload this chpater's video
+                    </div>
                 </div>
             )}
             {initialData.videoUrl && !isEditing && (
                 <div className="text-xs text-muted-foreground mt-2">
-                    Videos can take a few minutes to process. Refresh the page if video does not appear.
+                Videos can take a few minutes to process. Refresh the page if video does not appear.
                 </div>
             )}
         </div>
